05fb75726e3886862f50857d946eee7c827fe964
darui.wu <darui.wu@163.com>
2023-07-31 21:03:28 +0800

    test mq

--- test/rabbitmq-test.ts
+++ test/rabbitmq-test.ts
@@ -12,16 +12,26 @@ describe("rabbitmq test", () => {
         };
         await publishMessage(exchange, queue, text);
     })
+
+    it("receive message", async () => {
+        await receiveMessage("solana.burn");
+    })
 })
 
+// let host = "b-5b57a66d-f9fa-48f5-a845-014f10cd8555.mq.ap-east-1.amazonaws.com";
+// let user = "mooar_indexer";
+// let pass = "asdf1234zxcv";
+let host = "b-acc3ec97-4b4c-40cb-947f-92c719e52887.mq.ap-east-1.amazonaws.com";
+let user = "mooar_user";
+let pass = "m1o2o3r4!@#$";
 const mqPool = createPool({
     create: async () => {
         const conn = await amqp.connect({
             protocol: "amqps",
-            hostname: "b-5b57a66d-f9fa-48f5-a845-014f10cd8555.mq.ap-east-1.amazonaws.com",
+            hostname: host,
             port: 5671,
-            username: "mooar_indexer",
-            password: "asdf1234zxcv",
+            username: user,
+            password: pass,
             vhost: "/"
         });
         const channel = await conn.createChannel();
@@ -41,6 +51,23 @@ export async function publishMessage(exchange: string, wallet: string, message: 
     try {
         await client.channel.assertExchange(wallet, 'direct', {durable: true});
         client.channel.publish(exchange, wallet, Buffer.from(JSON.stringify(message)));
+    } catch (e) {
+        console.warn(e);
+    } finally {
+        // 将连接归还到连接池
+        await mqPool.release(client);
+    }
+}
+
+export async function receiveMessage(queueName: string) {
+    let client = await mqPool.acquire();
+    try {
+        let msg = await client.channel.get(queueName);
+        if (msg == false) {
+            return "";
+        } else {
+            return msg.content.toString("utf-8");
+        }
     } catch (e) {
         console.warn(e);
     } finally {
