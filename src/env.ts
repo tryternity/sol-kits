15e868dae73e8d57e6720af0cb38aa8365608e1b
daruiwu <wu.darui@boredflow.com>
2023-11-13 19:09:20 +0800

    "version": "0.0.37"

--- src/env.ts
+++ src/env.ts
@@ -1,39 +1,51 @@
 // noinspection JSUnusedGlobalSymbols
 
-import {Cluster as ClusterA, clusterApiUrl, Connection, Keypair} from "@solana/web3.js";
+import {clusterApiUrl, Connection, Keypair} from "@solana/web3.js";
 import {account} from "./account";
 import {Wallet} from "@project-serum/anchor";
 
-export type Cluster = ClusterA | "local";
+export enum Env {
+  local = 0,
+  devnet = 1,
+  mainnet = 2
+}
 
 export module env {
 
-    export function connection(cluster: Cluster = "devnet"): Connection {
-        let network = cluster == "local" ? "http://127.0.0.1:8899" : clusterApiUrl(cluster);
-        return new Connection(network, {
-            commitment: 'processed',
-            confirmTransactionInitialTimeout: 120 * 1000
-        });
+  export function cluster(env: Env = Env.devnet): string {
+    if (env == Env.local) {
+      return "http://127.0.0.1:8899";
+    } else {
+      return clusterApiUrl(env == Env.devnet ? "devnet" : "mainnet-beta");
     }
+  }
 
-    export let wallet: Keypair = account.localWallet();
-    export let defaultConnection: Connection = env.connection("devnet");
+  export function connection(env: Env = 1): Connection {
+    let network = cluster(env);
+    return new Connection(network, {
+      commitment: 'processed',
+      confirmTransactionInitialTimeout: 120 * 1000
+    });
+  }
 
-    export function setEnv(cluster?: Cluster, payer?: Keypair | Wallet): Connection {
-        if (cluster) {
-            defaultConnection = env.connection(cluster);
-        }
-        if (payer) {
-            wallet = payer instanceof Keypair ? payer : payer.payer;
-        }
-        return defaultConnection;
+  export let wallet: Keypair = account.localWallet();
+  export let defaultConnection: Connection = env.connection();
+
+  export function setEnv(cluster?: Env, payer?: Keypair | Wallet): Connection {
+    if (cluster) {
+      defaultConnection = env.connection(cluster);
     }
-
-    export function setConnection(connection: Connection) {
-        defaultConnection = connection;
+    if (payer) {
+      wallet = payer instanceof Keypair ? payer : payer.payer;
     }
+    return defaultConnection;
+  }
 
-    export function setPayer(payer: Keypair) {
-        wallet = payer;
-    }
+  export function setConnection(connection: Connection) {
+    defaultConnection = connection;
+  }
+
+  export function setPayer(payer: Keypair) {
+    wallet = payer;
+  }
 }
