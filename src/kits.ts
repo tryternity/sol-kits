b77f0548d585000145e67251ef49c437f215f7e0
daruiwu <wu.darui@boredflow.com>
2024-12-19 17:14:19 +0800

    create spl-404

--- src/kits.ts
+++ src/kits.ts
@@ -5,90 +5,100 @@ import crypto from "crypto";
 import {v4 as v4_uuid} from 'uuid';
 
 export const ePrint = (e: any) => {
-    console.log(e);
-    throw e;
+  console.log(e);
+  throw e;
 }
 
 export module kits {
-    export function json_replacer(key: any, value: any) {
-        if (value instanceof Map) {
-            let obj = {};
-            // @ts-ignore
-            Array.from(value.entries()).forEach(([k, v]) => obj[k] = v);
-            return obj;
-        } else if (value instanceof Buffer) {
-            return 'Buffer' + value.toString("hex");
-        } else if (value instanceof Array) {
-            return value.map(item => item instanceof Buffer ? "Buffer:" + item.toString("hex") : item);
-        } else if (typeof value === 'bigint') {
-            return value.toString(10);
-        } else {
-            return value;
-        }
+  export function json_replacer(key: any, value: any) {
+    if (value instanceof Map) {
+      let obj = {};
+      // @ts-ignore
+      Array.from(value.entries()).forEach(([k, v]) => obj[k] = v);
+      return obj;
+    } else if (value instanceof Buffer) {
+      return 'Buffer' + value.toString("hex");
+    } else if (value instanceof Array) {
+      return value.map(item => item instanceof Buffer ? "Buffer:" + item.toString("hex") : item);
+    } else if (typeof value === 'bigint') {
+      return value.toString(10);
+    } else {
+      return value;
     }
+  }
 
-    export function toJson(obj: any): string {
-        return JSON.stringify(obj, json_replacer);
-    }
+  export function toJson(obj: any): string {
+    return JSON.stringify(obj, json_replacer);
+  }
 
-    export function uuid(underline: boolean = false): string {
-        let uuid_str = v4_uuid();
-        return underline ? uuid_str : uuid_str.replace(/-/g, "");
-    }
+  export function uuid(underline: boolean = false): string {
+    let uuid_str = v4_uuid();
+    return underline ? uuid_str : uuid_str.replace(/-/g, "");
+  }
 
-    export function sha256(data: any): Buffer {
-        return crypto.createHash('sha256').update(data).digest()
-    }
+  export function sha256(data: any): Buffer {
+    return crypto.createHash('sha256').update(data).digest()
+  }
 
-    export function md5(str: string): Buffer {
-        return crypto.createHash("md5").update(str).digest();
-    }
+  export function md5(str: string): Buffer {
+    return crypto.createHash("md5").update(str).digest();
+  }
 
-    export function now(second: boolean = true): number {
-        return Math.floor(new Date().getTime() / (second ? 1000 : 1))
-    }
+  export function now(second: boolean = true): number {
+    return Math.floor(new Date().getTime() / (second ? 1000 : 1))
+  }
 
-    export function userHome(): string {
-        const os = require("os");
-        return os.homedir();
-    }
+  export function userHome(): string {
+    const os = require("os");
+    return os.homedir();
+  }
 
-    export async function post(url: string, body: any): Promise<String> {
-        let response = await fetch(url, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json;charset=utf-8'
-            },
-            body: JSON.stringify(body)
-        }).catch(ePrint);
-        return await text(response);
-    }
+  export async function post(url: string, body: any): Promise<String> {
+    let response = await fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json;charset=utf-8'
+      },
+      body: JSON.stringify(body)
+    }).catch(ePrint);
+    return await text(response);
+  }
 
-    export async function text(response: Response): Promise<String> {
-        if (response.body == null) {
-            return "<null>";
-        }
-        const chunks: Array<any> = [];
-        let reader = response.body.getReader();
-        while (true) {
-            let buff = await reader.read()
-            if (!buff || !buff.value) {
-                break
-            }
-            chunks.push(buff.value);
-        }
-        return Buffer.concat(chunks).toString("utf-8")
+  export async function text(response: Response): Promise<String> {
+    if (response.body == null) {
+      return "<null>";
     }
+    const chunks: Array<any> = [];
+    let reader = response.body.getReader();
+    while (true) {
+      let buff = await reader.read()
+      if (!buff || !buff.value) {
+        break
+      }
+      chunks.push(buff.value);
+    }
+    return Buffer.concat(chunks).toString("utf-8")
+  }
 
-    export function base64ToUint8Array(str: string): Uint8Array {
-        return Uint8Array.from(atob(str), (c) => c.charCodeAt(0));
-    }
+  export function base64ToUint8Array(str: string): Uint8Array {
+    return Uint8Array.from(atob(str), (c) => c.charCodeAt(0));
+  }
 
-    export function generate(size: number, generator: (index: number) => any): Array<any> {
-        let result = [];
-        for (let i = 0; i < size; i++) {
-            result.push(generator(i));
-        }
-        return result;
+  export function generate(size: number, generator: (index: number) => any): Array<any> {
+    let result = [];
+    for (let i = 0; i < size; i++) {
+      result.push(generator(i));
     }
+    return result;
+  }
+
+  export function printExplorerUrl(identifier: string, cluster: string = "devnet"): string {
+    if (!identifier) return '';
+    const baseUrl = 'https://solscan.io';
+    const localSuffix = !cluster ? "" : '?cluster=' + cluster;
+    const slug = identifier.length > 60 ? 'tx' : 'address';
+    let url = `${baseUrl}/${slug}/${identifier}${localSuffix}`;
+    console.log(url);
+    return url;
+  }
 }
--- test/spl404.ts
+++ test/spl404.ts
@@ -11,10 +11,30 @@ import {
   createV1, mintV1, mplTokenMetadata,
   TokenStandard,
 } from '@metaplex-foundation/mpl-token-metadata'
-import {env, ePrint, spl_404} from "../src";
+import {env, ePrint, kits, spl_404} from "../src";
 import {fromWeb3JsKeypair} from "@metaplex-foundation/umi-web3js-adapters";
 import bs58 from 'bs58';
 import {findAssociatedTokenPda} from "@metaplex-foundation/mpl-toolbox";
+import {
+  Connection,
+  Keypair,
+  sendAndConfirmTransaction,
+  SystemProgram,
+  Transaction
+} from "@solana/web3.js";
+import {
+  createAssociatedTokenAccountIdempotent,
+  createInitializeInstruction,
+  createInitializeMetadataPointerInstruction,
+  createInitializeMintInstruction,
+  createUpdateFieldInstruction,
+  ExtensionType,
+  getMintLen,
+  LENGTH_SIZE, mintTo,
+  TOKEN_2022_PROGRAM_ID,
+  TYPE_SIZE
+} from "@solana/spl-token";
+import {pack, TokenMetadata} from "@solana/spl-token-metadata";
 
 
 const SPL_TOKEN_2022_PROGRAM_ID: PublicKey = publicKey(
@@ -22,6 +42,11 @@ const SPL_TOKEN_2022_PROGRAM_ID: PublicKey = publicKey(
 )
 
 describe("SPL-404 test", () => {
+  beforeEach("init env", () => {
+    let ENDPOINT = "https://devnet.helius-rpc.com/?api-key=1a5deb03-d4bc-4b7a-b423-58d92148c0fe";
+    env.setEnv(ENDPOINT)
+  })
+
   it("create spl-404", async () => {
     const endpoint = "https://devnet.helius-rpc.com/?api-key=1a5deb03-d4bc-4b7a-b423-58d92148c0fe";
     console.log(env.wallet.publicKey.toBase58())
@@ -68,4 +93,96 @@ describe("SPL-404 test", () => {
     }).catch(ePrint)
     console.log(ret.mint.toBase58(), ret.signature)
   })
+
+  it("create spl-404 test3", async () => {
+    // 1. Create Token and Mint
+    const [initSig, mintSig] = await createTokenAndMint(env.defaultConnection, env.wallet);
+    console.log(`Token created and minted:`);
+    kits.printExplorerUrl(mintSig);
+    // console.log(`   ${kits.generateExplorerUrl(initSig)}`);
+    // console.log(`   ${kits.generateExplorerUrl(mintSig)}`);
+  })
 })
+
+async function createTokenAndMint(connection: Connection, wallet: Keypair): Promise<[string, string]> {
+
+  // After calculating the minimum balance for the mint account...
+  let mintKeypair = Keypair.generate();
+  let mint = mintKeypair.publicKey;
+  const tokenMetadata: TokenMetadata = {
+    updateAuthority: wallet.publicKey,
+    mint: mint,
+    name: 'QN Pixel',
+    symbol: 'QNPIX',
+    uri: "https://qn-shared.quicknode-ipfs.com/ipfs/QmQFh6WuQaWAMLsw9paLZYvTsdL5xJESzcoSxzb6ZU3Gjx",
+    additionalMetadata: [["Background", "Blue"], ["WrongData", "DeleteMe!"], ["Points", "0"]],
+  };
+  const mintLen = getMintLen([ExtensionType.MetadataPointer]);
+  const metadataLen = TYPE_SIZE + LENGTH_SIZE + pack(tokenMetadata).length;
+  const mintLamports = await connection.getMinimumBalanceForRentExemption(mintLen + metadataLen);
+
+  let authority = wallet;
+  let payer = wallet;
+  let owner = wallet;
+  // Prepare transaction
+  const transaction = new Transaction().add(
+      SystemProgram.createAccount({
+        fromPubkey: payer.publicKey,
+        newAccountPubkey: mint,
+        space: mintLen,
+        lamports: mintLamports,
+        programId: TOKEN_2022_PROGRAM_ID,
+      }),
+      createInitializeMetadataPointerInstruction(
+          mint,
+          authority.publicKey,
+          mint,
+          TOKEN_2022_PROGRAM_ID,
+      ),
+      createInitializeMintInstruction(
+          mint,
+          0,
+          authority.publicKey,
+          null,
+          TOKEN_2022_PROGRAM_ID,
+      ),
+      createInitializeInstruction({
+        programId: TOKEN_2022_PROGRAM_ID,
+        metadata: mint,
+        updateAuthority: authority.publicKey,
+        mint: mint,
+        mintAuthority: authority.publicKey,
+        name: tokenMetadata.name,
+        symbol: tokenMetadata.symbol,
+        uri: tokenMetadata.uri,
+      }),
+      createUpdateFieldInstruction({
+        programId: TOKEN_2022_PROGRAM_ID,
+        metadata: mint,
+        updateAuthority: authority.publicKey,
+        field: tokenMetadata.additionalMetadata[0][0],
+        value: tokenMetadata.additionalMetadata[0][1],
+      }),
+      createUpdateFieldInstruction({
+        programId: TOKEN_2022_PROGRAM_ID,
+        metadata: mint,
+        updateAuthority: authority.publicKey,
+        field: tokenMetadata.additionalMetadata[1][0],
+        value: tokenMetadata.additionalMetadata[1][1],
+      }),
+      createUpdateFieldInstruction({
+        programId: TOKEN_2022_PROGRAM_ID,
+        metadata: mint,
+        updateAuthority: authority.publicKey,
+        field: tokenMetadata.additionalMetadata[2][0],
+        value: tokenMetadata.additionalMetadata[2][1],
+      }),
+  );
+  // Initialize NFT with metadata
+  const initSig = await sendAndConfirmTransaction(connection, transaction, [payer, mintKeypair, authority]);
+  // Create associated token account
+  const sourceAccount = await createAssociatedTokenAccountIdempotent(connection, payer, mint, owner.publicKey, {}, TOKEN_2022_PROGRAM_ID);
+  // Mint NFT to associated token account
+  const mintSig = await mintTo(connection, payer, mint, sourceAccount, authority, 1, [], undefined, TOKEN_2022_PROGRAM_ID);
+  return [initSig, mintSig];
+}
