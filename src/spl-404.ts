5cb1b74223b4a0920403f41dd5e14dce5e7c5bcc
daruiwu <wu.darui@boredflow.com>
2024-12-19 17:29:15 +0800

    createTokenAndMint

--- src/spl-404.ts
+++ src/spl-404.ts
@@ -4,12 +4,130 @@ import {
   generateSigner,
   percentAmount
 } from "@metaplex-foundation/umi";
-import {ePrint} from "./kits";
-import {PublicKey} from "@solana/web3.js";
+import {ePrint, kits} from "./kits";
+import {
+  Keypair,
+  PublicKey,
+  sendAndConfirmTransaction,
+  SystemProgram,
+  Transaction
+} from "@solana/web3.js";
 import bs58 from "bs58";
-import {getOrCreateAssociatedTokenAccount, mintTo, TOKEN_2022_PROGRAM_ID} from "@solana/spl-token";
+import {
+  createAssociatedTokenAccountIdempotent,
+  createInitializeInstruction,
+  createInitializeMetadataPointerInstruction,
+  createInitializeMintInstruction,
+  createUpdateFieldInstruction,
+  ExtensionType,
+  getMintLen,
+  getOrCreateAssociatedTokenAccount,
+  LENGTH_SIZE,
+  mintTo,
+  TOKEN_2022_PROGRAM_ID,
+  TYPE_SIZE
+} from "@solana/spl-token";
+import {pack, TokenMetadata} from "@solana/spl-token-metadata";
 
 export module spl_404 {
+  export type Metadata = {
+    name?: string,
+    symbol?: string,
+    uri?: string,
+    additionalMetadata?: (readonly [string, string])[]
+  };
+  export type Spl404Ret = {
+    mint: PublicKey,
+    initSig: string,
+    mintSig: string
+  }
+
+  export async function createTokenAndMint(wallet: Keypair, meta?: Metadata): Promise<Spl404Ret> {
+    // After calculating the minimum balance for the mint account...
+    let mintKeypair = Keypair.generate();
+    let mint = mintKeypair.publicKey;
+    let authority = wallet;
+    let payer = wallet;
+    let owner = wallet;
+
+    const tokenMetadata: TokenMetadata = {
+      updateAuthority: authority.publicKey,
+      mint: mint,
+      name: meta?.name ?? 'QN Pixel',
+      symbol: meta?.symbol ?? 'QNPIX',
+      uri: meta?.uri ?? "https://qn-shared.quicknode-ipfs.com/ipfs/QmQFh6WuQaWAMLsw9paLZYvTsdL5xJESzcoSxzb6ZU3Gjx",
+      additionalMetadata: meta?.additionalMetadata ?? [["Background", "Blue"], ["WrongData", "DeleteMe!"], ["Points", "0"]],
+    };
+    const mintLen = getMintLen([ExtensionType.MetadataPointer]);
+    const metadataLen = TYPE_SIZE + LENGTH_SIZE + pack(tokenMetadata).length;
+    const mintLamports = await env.defaultConnection.getMinimumBalanceForRentExemption(mintLen + metadataLen);
+
+    // Prepare transaction
+    const transaction = new Transaction().add(
+        SystemProgram.createAccount({
+          fromPubkey: payer.publicKey,
+          newAccountPubkey: mint,
+          space: mintLen,
+          lamports: mintLamports,
+          programId: TOKEN_2022_PROGRAM_ID,
+        }),
+        createInitializeMetadataPointerInstruction(
+            mint,
+            authority.publicKey,
+            mint,
+            TOKEN_2022_PROGRAM_ID,
+        ),
+        createInitializeMintInstruction(
+            mint,
+            0,
+            authority.publicKey,
+            null,
+            TOKEN_2022_PROGRAM_ID,
+        ),
+        createInitializeInstruction({
+          programId: TOKEN_2022_PROGRAM_ID,
+          metadata: mint,
+          updateAuthority: authority.publicKey,
+          mint: mint,
+          mintAuthority: authority.publicKey,
+          name: tokenMetadata.name,
+          symbol: tokenMetadata.symbol,
+          uri: tokenMetadata.uri,
+        }),
+        createUpdateFieldInstruction({
+          programId: TOKEN_2022_PROGRAM_ID,
+          metadata: mint,
+          updateAuthority: authority.publicKey,
+          field: tokenMetadata.additionalMetadata[0][0],
+          value: tokenMetadata.additionalMetadata[0][1],
+        }),
+        createUpdateFieldInstruction({
+          programId: TOKEN_2022_PROGRAM_ID,
+          metadata: mint,
+          updateAuthority: authority.publicKey,
+          field: tokenMetadata.additionalMetadata[1][0],
+          value: tokenMetadata.additionalMetadata[1][1],
+        }),
+        createUpdateFieldInstruction({
+          programId: TOKEN_2022_PROGRAM_ID,
+          metadata: mint,
+          updateAuthority: authority.publicKey,
+          field: tokenMetadata.additionalMetadata[2][0],
+          value: tokenMetadata.additionalMetadata[2][1],
+        }),
+    );
+    // Initialize NFT with metadata
+    const initSig = await sendAndConfirmTransaction(env.defaultConnection, transaction, [payer, mintKeypair, authority]);
+    // Create associated token account
+    const sourceAccount = await createAssociatedTokenAccountIdempotent(env.defaultConnection, payer, mint, owner.publicKey, {}, TOKEN_2022_PROGRAM_ID);
+    // Mint NFT to associated token account
+    const mintSig = await mintTo(env.defaultConnection, payer, mint, sourceAccount, authority, 1, [], undefined, TOKEN_2022_PROGRAM_ID);
+    console.log("mint:" + mint.toBase58());
+    kits.printExplorerUrl(initSig);
+    kits.printExplorerUrl(mintSig);
+    return {mint, initSig, mintSig}
+  }
+
   export async function createMpl404(arg: {
     env?: Env | string
     nftName: string,
@@ -40,10 +158,11 @@ export module spl_404 {
         TOKEN_2022_PROGRAM_ID,
         undefined).catch(ePrint);
     console.log("ata:", ata);
-    let signature = await mintTo(env.defaultConnection, env.wallet, _mint, ata.address, env.wallet, 1, [], undefined, TOKEN_2022_PROGRAM_ID).catch(ePrint);
+    let initSig = bs58.encode(ret.signature);
+    let mintSig = await mintTo(env.defaultConnection, env.wallet, _mint, ata.address, env.wallet, 1, [], undefined, TOKEN_2022_PROGRAM_ID).catch(ePrint);
     return {
       mint: _mint,
-      signature: [bs58.encode(ret.signature), signature],
+      signature: [initSig, mintSig],
     }
   }
 }
--- test/spl404.ts
+++ test/spl404.ts
@@ -1,41 +1,22 @@
 import {
   createSignerFromKeypair,
-  keypairIdentity,
   generateSigner,
+  keypairIdentity,
   percentAmount,
   publicKey,
   PublicKey
 } from '@metaplex-foundation/umi'
 import {createUmi} from '@metaplex-foundation/umi-bundle-defaults'
 import {
-  createV1, mintV1, mplTokenMetadata,
+  createV1,
+  mintV1,
+  mplTokenMetadata,
   TokenStandard,
 } from '@metaplex-foundation/mpl-token-metadata'
-import {env, ePrint, kits, spl_404} from "../src";
+import {env, ePrint, spl_404} from "../src";
 import {fromWeb3JsKeypair} from "@metaplex-foundation/umi-web3js-adapters";
 import bs58 from 'bs58';
 import {findAssociatedTokenPda} from "@metaplex-foundation/mpl-toolbox";
-import {
-  Connection,
-  Keypair,
-  sendAndConfirmTransaction,
-  SystemProgram,
-  Transaction
-} from "@solana/web3.js";
-import {
-  createAssociatedTokenAccountIdempotent,
-  createInitializeInstruction,
-  createInitializeMetadataPointerInstruction,
-  createInitializeMintInstruction,
-  createUpdateFieldInstruction,
-  ExtensionType,
-  getMintLen,
-  LENGTH_SIZE, mintTo,
-  TOKEN_2022_PROGRAM_ID,
-  TYPE_SIZE
-} from "@solana/spl-token";
-import {pack, TokenMetadata} from "@solana/spl-token-metadata";
-
 
 const SPL_TOKEN_2022_PROGRAM_ID: PublicKey = publicKey(
     'TokenzQdBNbLqP5VEhdkAS6EPFLC1PHnBqCXEpPxuEb'
@@ -96,93 +77,7 @@ describe("SPL-404 test", () => {
 
   it("create spl-404 test3", async () => {
     // 1. Create Token and Mint
-    const [initSig, mintSig] = await createTokenAndMint(env.defaultConnection, env.wallet);
-    console.log(`Token created and minted:`);
-    kits.printExplorerUrl(mintSig);
-    // console.log(`   ${kits.generateExplorerUrl(initSig)}`);
-    // console.log(`   ${kits.generateExplorerUrl(mintSig)}`);
+    await spl_404.createTokenAndMint(env.wallet);
   })
 })
 
-async function createTokenAndMint(connection: Connection, wallet: Keypair): Promise<[string, string]> {
-
-  // After calculating the minimum balance for the mint account...
-  let mintKeypair = Keypair.generate();
-  let mint = mintKeypair.publicKey;
-  const tokenMetadata: TokenMetadata = {
-    updateAuthority: wallet.publicKey,
-    mint: mint,
-    name: 'QN Pixel',
-    symbol: 'QNPIX',
-    uri: "https://qn-shared.quicknode-ipfs.com/ipfs/QmQFh6WuQaWAMLsw9paLZYvTsdL5xJESzcoSxzb6ZU3Gjx",
-    additionalMetadata: [["Background", "Blue"], ["WrongData", "DeleteMe!"], ["Points", "0"]],
-  };
-  const mintLen = getMintLen([ExtensionType.MetadataPointer]);
-  const metadataLen = TYPE_SIZE + LENGTH_SIZE + pack(tokenMetadata).length;
-  const mintLamports = await connection.getMinimumBalanceForRentExemption(mintLen + metadataLen);
-
-  let authority = wallet;
-  let payer = wallet;
-  let owner = wallet;
-  // Prepare transaction
-  const transaction = new Transaction().add(
-      SystemProgram.createAccount({
-        fromPubkey: payer.publicKey,
-        newAccountPubkey: mint,
-        space: mintLen,
-        lamports: mintLamports,
-        programId: TOKEN_2022_PROGRAM_ID,
-      }),
-      createInitializeMetadataPointerInstruction(
-          mint,
-          authority.publicKey,
-          mint,
-          TOKEN_2022_PROGRAM_ID,
-      ),
-      createInitializeMintInstruction(
-          mint,
-          0,
-          authority.publicKey,
-          null,
-          TOKEN_2022_PROGRAM_ID,
-      ),
-      createInitializeInstruction({
-        programId: TOKEN_2022_PROGRAM_ID,
-        metadata: mint,
-        updateAuthority: authority.publicKey,
-        mint: mint,
-        mintAuthority: authority.publicKey,
-        name: tokenMetadata.name,
-        symbol: tokenMetadata.symbol,
-        uri: tokenMetadata.uri,
-      }),
-      createUpdateFieldInstruction({
-        programId: TOKEN_2022_PROGRAM_ID,
-        metadata: mint,
-        updateAuthority: authority.publicKey,
-        field: tokenMetadata.additionalMetadata[0][0],
-        value: tokenMetadata.additionalMetadata[0][1],
-      }),
-      createUpdateFieldInstruction({
-        programId: TOKEN_2022_PROGRAM_ID,
-        metadata: mint,
-        updateAuthority: authority.publicKey,
-        field: tokenMetadata.additionalMetadata[1][0],
-        value: tokenMetadata.additionalMetadata[1][1],
-      }),
-      createUpdateFieldInstruction({
-        programId: TOKEN_2022_PROGRAM_ID,
-        metadata: mint,
-        updateAuthority: authority.publicKey,
-        field: tokenMetadata.additionalMetadata[2][0],
-        value: tokenMetadata.additionalMetadata[2][1],
-      }),
-  );
-  // Initialize NFT with metadata
-  const initSig = await sendAndConfirmTransaction(connection, transaction, [payer, mintKeypair, authority]);
-  // Create associated token account
-  const sourceAccount = await createAssociatedTokenAccountIdempotent(connection, payer, mint, owner.publicKey, {}, TOKEN_2022_PROGRAM_ID);
-  // Mint NFT to associated token account
-  const mintSig = await mintTo(connection, payer, mint, sourceAccount, authority, 1, [], undefined, TOKEN_2022_PROGRAM_ID);
-  return [initSig, mintSig];
-}
